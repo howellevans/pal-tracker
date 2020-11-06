bf4d924fd58b742607ac5a5888658e3c4b99e1cb
Howell Evans <howell.evans1@gmail.com>
2020-11-06 19:50:54 +0000

    Updated to include test database and change how the datasource is created.

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -1,9 +1,10 @@
 package io.pivotal.pal.tracker;
 
-import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -13,9 +14,7 @@ public class PalTrackerApplication {
     }
 
     @Bean(name = {"TimeEntryRepository"})
-    public TimeEntryRepository getInMemoryTimeEntryRepository() {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setURL(System.getenv("SPRING_DATASOURCE_URL"));
+    public TimeEntryRepository getInMemoryTimeEntryRepository(DataSource dataSource) {
         return new JdbcTimeEntryRepository(dataSource);
     }
 }
